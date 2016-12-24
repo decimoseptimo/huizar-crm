655871b9e0864226aabe08a08d9f636a6e199763
Decimoseptimo <decimoseptimo@gmail.com>
2016-12-24 14:36:25 -0800

    Add REST interface

---
+++ api/config/api.php
@@ -0,0 +1,57 @@
+<?php
+
+//$params = require(__DIR__ . '/params.php');
+
+$config = [
+    'id' => 'api',
+    'basePath' => dirname(__DIR__).'/..',
+    'bootstrap' => ['log'],
+    'components' => [
+        /*'urlManager'  => [
+            'enablePrettyUrl'  => true,
+            'showScriptName'  => false,
+            'rules' => [
+                [
+                    'class'  => 'yii\rest\UrlRule',
+                    'controller'  => [
+                        'v1/user'
+                    ],
+                ],
+            ],
+        ],*/
+        'request' => [
+            'parsers' => [
+                'application/json'  => 'yii\web\JsonParser',
+            ]
+        ],
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'user' => [
+            'identityClass' => 'app\models\User',
+            'enableAutoLogin' => false,
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                    'logFile'  => '@api/runtime/logs/error.log',
+                ],
+            ],
+        ],
+        'db' => require(__DIR__ . '/../../config/db.php'),
+    ],
+    'modules' => [
+        'v1' => [
+            'class' => 'app\api\modules\v1\Module',
+        ],
+    ],
+    'aliases' => [
+        '@api' => dirname(dirname(__DIR__)) . '/api',
+    ],
+    //'params' => $params,
+];
+
+return $config;
---
+++ api/modules/v1/controllers/CustomerController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace app\api\modules\v1\controllers;
+
+use yii\rest\ActiveController;
+use yii\web\Controller;
+
+/**
+ * Default controller for the `v1` module
+ */
+class CustomerController extends ActiveController
+{
+
+    public $modelClass = 'app\models\Customer';
+    /**
+     * Renders the index view for the module
+     * @return string
+     */
+    /*public function actionIndex()
+    {
+        return $this->render('index');
+    }*/
+
+}
